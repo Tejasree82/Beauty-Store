35ff36e29985e0fc39c01dc70e7796f79ed606e0
tejasree <you@example.com>
2024-09-30 12:04:09 +0530

    Initial commit

---
+++ src/app/customer.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  getAllCountries(): any {
+    return this.http.get('https://restcountries.com/v3.1/all');
+  }
+
+  loginStatus: boolean;
+
+  constructor(private http: HttpClient) { this.loginStatus = false; }
+
+
+  registerCustomer(customer: any){
+    return this.http.post('http://localhost:8090/registerCustomer', customer);
+  }
+
+  customerLogin(emailId:any, password:any){
+    return this.http.get('http://localhost:8090/customerLogin/'+ emailId + '/' + password).toPromise();
+  }
+
+  getCustomerByEmail(emailId: any){
+    return this.http.get('http://localhost:8090/getCustomerByEmail/'+ emailId);
+  }
+
+  sendOtpToEmail(emailId: any){
+    return this.http.get('http://localhost:8090/sendOtpToEmail/'+ emailId);
+  }
+
+  updateCustomerPassword(customer: any){
+    return this.http.put('http://localhost:8090/updateCustomerPassword', customer);
+  }
+  isCustomerLoggedIn(){
+    this.loginStatus = true;
+  }
+  isCustomerLoggedOut(){
+    this.loginStatus = false;
+  }
+  getCustomerLoginStatus(){
+    return this.loginStatus;
+  }
+}
